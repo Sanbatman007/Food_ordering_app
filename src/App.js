8c27f701e41a22a5c8ec2c519a692ab20facf088
Sanket Kad <sanket.kad@hcl.com>
2023-07-11 10:31:18 +0530

    Code changes for food ordering app Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./CompanyLogo.png";
+import "./App.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={logo} className="logo-image" />
+      </div>
+      <div>
+        <ul className="header-list">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+
+const RestroCard = () => {
+  return (
+    <div>Card goes here</div>
+  );
+}
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-container">Search Goes here</div>
+      <div className="resto-container">
+        <RestroCard/>
+      </div>
+    </div>
+  );
+  
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Body />
     </div>
   );
 }
